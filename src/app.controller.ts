1100a07a4b3ed8b9f98d0001cfeb4f7a00fe457b
okonomiyakki <parkji12532@naver.com>
2024-01-23 01:36:50 +0900

    feat: main.hbs 반환 전용 root 컨트롤러 작성
    - env 파일로부터 host url을 hbs 파일로 전달

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Render } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly config: ConfigService,
+  ) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  private HostBaseUrl = this.config.get('HOST_BASE_URL');
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('main')
+  root() {
+    return {
+      data: {
+        HostBaseUrl: this.HostBaseUrl,
+      },
+    };
   }
 }
