5d39fbf3ac6ac4e9bf3c84fb7495fa1282487323
okonomiyakki <parkji12532@naver.com>
2024-01-23 02:50:51 +0900

    feat: 'lol' 모듈에 http 모듈 import

--- src/lol/lol.module.ts
+++ src/lol/lol.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LolController } from './lol.controller';
 import { LolService } from './lol.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
+  imports: [HttpModule],
   controllers: [LolController],
-  providers: [LolService]
+  providers: [LolService],
 })
 export class LolModule {}
