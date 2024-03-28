43ff7071c1f096f33386b9fa47cd6ecd38884e60
okonomiyakki <parkji12532@naver.com>
2024-03-29 03:18:29 +0900

    fix: 점검용 블락

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,48 +1,49 @@
 const summonersInfoRequest = () => {
-  const hostBaseUrl = document.getElementById('host').dataset.hostBaseUrl;
+  alert('점검중');
+  // const hostBaseUrl = document.getElementById('host').dataset.hostBaseUrl;
 
-  const summonersName = document.getElementById('summonersNameInput').value;
-  const summonersTag = document.getElementById('summonersTagInput').value;
+  // const summonersName = document.getElementById('summonersNameInput').value;
+  // const summonersTag = document.getElementById('summonersTagInput').value;
 
-  const title = document.getElementById('title');
-  const loading = document.getElementById('loading');
+  // const title = document.getElementById('title');
+  // const loading = document.getElementById('loading');
 
-  loading.style.display = 'flex';
+  // loading.style.display = 'flex';
 
-  axios
-    .post(`${hostBaseUrl}/lol`, {
-      summonersName: summonersName,
-      summonersTag: summonersTag,
-    })
-    .then((res) => {
-      console.log('서버 응답: ', res.data);
+  // axios
+  //   .post(`${hostBaseUrl}/lol`, {
+  //     summonersName: summonersName,
+  //     summonersTag: summonersTag,
+  //   })
+  //   .then((res) => {
+  //     console.log('서버 응답: ', res.data);
 
-      if (res.data.summonersEncryptedId) {
-        const infoData = {
-          summonersName,
-          summonersTag,
-          summonersEncryptedId: res.data.summonersEncryptedId,
-          summonersInfo: res.data.summonersInfo,
-        };
+  //     if (res.data.summonersEncryptedId) {
+  //       const infoData = {
+  //         summonersName,
+  //         summonersTag,
+  //         summonersEncryptedId: res.data.summonersEncryptedId,
+  //         summonersInfo: res.data.summonersInfo,
+  //       };
 
-        localStorage.setItem('infoData', JSON.stringify(infoData));
+  //       localStorage.setItem('infoData', JSON.stringify(infoData));
 
-        window.location.href = `${hostBaseUrl}/summoners`;
-      } else {
-        if (res.data.errorCode === 403) title.innerHTML = res.data.message;
-        else if (res.data.errorCode === 400) title.innerHTML = res.data.message;
-        else if (res.data.errorCode === 404) title.innerHTML = res.data.message;
-        else title.innerHTML = res.data.message;
-      }
-    })
-    .catch((error) => {
-      console.error('[Client] 소환사 검색 에러:', error);
-    })
-    .finally(() => {
-      loading.style.display = 'none';
-    });
+  //       window.location.href = `${hostBaseUrl}/summoners`;
+  //     } else {
+  //       if (res.data.errorCode === 403) title.innerHTML = res.data.message;
+  //       else if (res.data.errorCode === 400) title.innerHTML = res.data.message;
+  //       else if (res.data.errorCode === 404) title.innerHTML = res.data.message;
+  //       else title.innerHTML = res.data.message;
+  //     }
+  //   })
+  //   .catch((error) => {
+  //     console.error('[Client] 소환사 검색 에러:', error);
+  //   })
+  //   .finally(() => {
+  //     loading.style.display = 'none';
+  //   });
 };
 
 const loginForRiotAccount = () => {
-  alert('점검 중입니다.');
+  alert('준비중');
 };
