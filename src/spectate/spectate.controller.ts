322a9458d10566052a5baf0f7ebc609627f60f5f
okonomiyakki <parkji12532@naver.com>
2024-03-31 23:41:48 +0900

    fix: api 엔드포인트 수정(기능수정x)

--- src/spectate/spectate.controller.ts
+++ src/spectate/spectate.controller.ts
@@ -9,17 +9,17 @@ import { SpectateService } from './spectate.service';
 import { SearchSummonerDto } from './dto/search-summoner.dto';
 import { SpectateSummonerDto } from './dto/spectate-summoner.dto';
 
-@Controller('api/v1.0')
+@Controller('api')
 export class SpectateController {
   constructor(private spectateService: SpectateService) {}
 
-  @Post('/summoners')
+  @Post('/v1.0/summoners')
   @UsePipes(ValidationPipe)
   getSummonersEncryptedId(@Body() searchSummonerDto: SearchSummonerDto) {
     return this.spectateService.getSummonersEncryptedId(searchSummonerDto);
   }
 
-  @Post('/spectate/live')
+  @Post('/v1.0/spectate/live')
   @UsePipes(ValidationPipe)
   getLiveGameTime(@Body() spectateSummonerDto: SpectateSummonerDto) {
     return this.spectateService.getLiveGameTime(spectateSummonerDto);
