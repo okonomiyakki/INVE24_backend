37350f0f992a2fcf204a22ca1be066644435db15
okonomiyakki <parkji12532@naver.com>
2024-04-08 21:10:15 +0900

    feat: spectator API 두개로 쪼갬
    -밴픽이랑 로딩 구분하려고

--- src/spectate/spectate.controller.ts
+++ src/spectate/spectate.controller.ts
@@ -2,26 +2,45 @@ import {
   Controller,
   Post,
   Body,
+  Res,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { Response } from 'express';
 import { SpectateService } from './spectate.service';
-import { SearchSummonerDto } from './dto/search-summoner.dto';
 import { SpectateSummonerDto } from './dto/spectate-summoner.dto';
 
-@Controller('api')
+@Controller('api/v2.0/spectate')
 export class SpectateController {
   constructor(private spectateService: SpectateService) {}
 
-  @Post('/v1.0/summoners')
+  // @Post('/v1.0/summoners')
+  // @UsePipes(ValidationPipe)
+  // getSummonersEncryptedId(@Body() searchSummonerDto: SearchSummonerDto) {
+  //   return this.spectateService.getSummonersEncryptedId(searchSummonerDto);
+  // }
+
+  // @Post('/v1.0/spectate/live')
+  // @UsePipes(ValidationPipe)
+  // getLiveGameTime(@Body() spectateSummonerDto: SpectateSummonerDto) {
+  //   return this.spectateService.getLiveGameTime(spectateSummonerDto);
+  // }
+
+  @Post('/status')
   @UsePipes(ValidationPipe)
-  getSummonersEncryptedId(@Body() searchSummonerDto: SearchSummonerDto) {
-    return this.spectateService.getSummonersEncryptedId(searchSummonerDto);
+  getCurrentGameStatusHandler(
+    @Body() spectateSummonerDto: SpectateSummonerDto,
+    @Res() res: Response,
+  ) {
+    return this.spectateService.getCurrentGameStatus(spectateSummonerDto, res);
   }
 
-  @Post('/v1.0/spectate/live')
+  @Post('/live')
   @UsePipes(ValidationPipe)
-  getLiveGameTime(@Body() spectateSummonerDto: SpectateSummonerDto) {
-    return this.spectateService.getLiveGameTime(spectateSummonerDto);
+  getCurrentGameHandler(
+    @Body() spectateSummonerDto: SpectateSummonerDto,
+    @Res() res: Response,
+  ) {
+    return this.spectateService.getCurrentGame(spectateSummonerDto, res);
   }
 }
