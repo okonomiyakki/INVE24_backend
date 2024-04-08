41b8bc80745dca7a2d10ee3b10264506896f3c6d
okonomiyakki <parkji12532@naver.com>
2024-04-08 21:05:10 +0900

    feat: 밴픽 타이머 기능 추가
    - 밴픽, 로딩, 시작 타이머를 해당 페이지에서 모두 구현하도록 수정

--- src/public/js/timer.js
+++ src/public/js/timer.js
@@ -1,52 +1,138 @@
 const hostBaseUrl = document.getElementById('host').dataset.hostBaseUrl;
-const title = document.getElementById('title');
-const gameStartTime = document.getElementById('startTime');
+
+let intervalId;
 
 document.addEventListener('DOMContentLoaded', function () {
-  const storedInfoData = localStorage.getItem('infoData');
-  if (storedInfoData) {
-    const infoData = JSON.parse(storedInfoData);
+  const storedTokenInfo = localStorage.getItem('tokenInfo');
+  const storedLeagueInfo = localStorage.getItem('leagueInfo');
 
-    title.innerHTML = `'${infoData.summonersName}'<br>님의 게임이 시작되었습니다.<br>타이머는 2분 뒤 자동 종료됩니다.`;
-  } else {
-    console.error(`infoData가 존재하지 않습니다.`);
-  }
-
-  const storedTimeData = localStorage.getItem('timeData');
-
-  if (storedTimeData) {
-    const timeData = JSON.parse(storedTimeData);
-
-    console.log('timeData: ', timeData);
-
-    const hours = timeData.gameStartTime.hours;
-    const minutes = timeData.gameStartTime.minutes;
-    const seconds = timeData.gameStartTime.seconds;
-
-    const h = hours < 10 ? `0${hours}` : hours;
-    const m = minutes < 10 ? `0${minutes}` : minutes;
-    const s = seconds < 10 ? `0${seconds}` : seconds;
-
-    gameStartTime.innerText = `${h}:${m}:${s}`;
-
-    startAutoIncrement(timeData.realTimeSeconds, 1);
-  } else {
-    console.error(`timeData가 존재하지 않습니다.`);
+  if (!storedTokenInfo) {
     alert('잘못된 접근입니다.');
-    window.location.href = `${hostBaseUrl}`;
+
+    replaceLocation(`${hostBaseUrl}`);
   }
+
+  let retryCount = 0;
+
+  /** 5분 */
+  const MAX_RETRIES = 30;
+
+  const leagueInfo = JSON.parse(storedLeagueInfo);
+
+  currentGameStatusFetcher(leagueInfo, retryCount, MAX_RETRIES);
 });
 
-const back = () => {
-  localStorage.removeItem('timeData');
-  window.location.href = `${hostBaseUrl}/summoners`;
+const currentGameStatusFetcher = (leagueInfo, retryCount, MAX_RETRIES) => {
+  axios
+    .post(`${hostBaseUrl}/api/v2.0/spectate/status`, {
+      summonerName: leagueInfo.summonerName,
+      summonerTag: leagueInfo.summonerTag,
+      encryptedSummonerId: leagueInfo.encryptedSummonerId,
+    })
+    .then((res) => {
+      const { gameStartTime } = res.data.data;
+
+      console.log(
+        '로딩 시작 : ',
+        `${gameStartTime.hours}시 ${gameStartTime.minutes}분 ${gameStartTime.seconds}초`,
+      );
+
+      injectHTML(
+        'title',
+        `'${leagueInfo.summonerName}'<br>님의 게임은 현재 로딩중입니다.<br>곧 게임이 시작됩니다.`,
+      );
+
+      injectHTML('banpick', '밴픽 완료');
+
+      hideAnimation('banpick');
+
+      showElement('load');
+
+      injectHTML('loading', '로딩중');
+
+      showAnimation('loading');
+
+      if (intervalId) clearInterval(intervalId);
+
+      generateTimer(0, 'realTime', false);
+
+      currentGameFetcher();
+    })
+    .catch((error) => {
+      if (error.response.status === 404 && retryCount < MAX_RETRIES) {
+        injectHTML(
+          'title',
+          `'${leagueInfo.summonerName}'<br>님의 게임은 현재 밴픽중입니다.<br>곧 로딩이 시작됩니다.`,
+        );
+
+        hideElement('load');
+
+        showElement('pick');
+
+        injectHTML('banpick', '밴픽중');
+
+        showAnimation('banpick');
+
+        if (!intervalId) generateTimer(0, 'pickTime', false);
+
+        retryCount++;
+
+        setTimeout(
+          () => currentGameStatusFetcher(retryCount, MAX_RETRIES),
+          10000,
+        );
+      } else {
+        alert(error.response.message);
+
+        handleRedirectPrev();
+      }
+    });
 };
 
-// const reset = () => {
-//   localStorage.removeItem('infoData');
-//   localStorage.removeItem('timeData');
-//   window.location.href = `${hostBaseUrl}`;
-// };
+const currentGameFetcher = () => {
+  axios
+    .post(`${hostBaseUrl}/api/v2.0/spectate/live`, {
+      summonerName: leagueInfo.summonerName,
+      summonerTag: leagueInfo.summonerTag,
+      encryptedSummonerId: leagueInfo.encryptedSummonerId,
+    })
+    .then((res) => {
+      const { gameStartTime, realTimeSeconds } = res.data.data;
+
+      injectHTML(
+        'title',
+        `'${leagueInfo.summonerName}'<br>님의 게임이 시작되었습니다.<br>2분 후 이전 페이지로 돌아갑니다.`,
+      );
+
+      console.log(
+        '게임 시작 : ',
+        `${gameStartTime.hours}시 ${gameStartTime.minutes}분 ${gameStartTime.seconds}초`,
+      );
+
+      console.log('게임 경과 시간 : ', `${realTimeSeconds}초 경과`);
+
+      injectHTML('loading', '로딩 완료');
+
+      hideAnimation('loading');
+
+      hideElement('pick');
+
+      injectHTML('loading', '협곡 시간');
+
+      if (intervalId) clearInterval(intervalId);
+
+      generateTimer(realTimeSeconds, 'realTime', true);
+    })
+    .catch((error) => {
+      alert(error.response.message);
+
+      handleRedirectPrev();
+    });
+};
+
+const handleRedirectPrev = () => {
+  replaceLocation(`${hostBaseUrl}/summoners`);
+};
 
 const convertSecondsToHMS = (seconds) => {
   var hours = Math.floor(seconds / 3600);
@@ -60,13 +146,11 @@ const convertSecondsToHMS = (seconds) => {
   return { h, m, s };
 };
 
-const updateCounter = (h, m, s) => {
-  document.getElementById('realTime').innerText = `${h}:${m}:${s}`;
+const updateTimer = (element, h, m, s) => {
+  document.getElementById(element).innerText = `${h}:${m}:${s}`;
 };
 
-let intervalId;
-
-const startAutoIncrement = (num) => {
+const generateTimer = (num, element, offset) => {
   let currentCounter = num;
 
   if (intervalId) {
@@ -76,12 +160,40 @@ const startAutoIncrement = (num) => {
   intervalId = setInterval(() => {
     currentCounter++;
 
-    const h = convertSecondsToHMS(currentCounter).h;
-    const m = convertSecondsToHMS(currentCounter).m;
-    const s = convertSecondsToHMS(currentCounter).s;
-    if (currentCounter === 120) {
+    const { h, m, s } = convertSecondsToHMS(currentCounter);
+
+    if (currentCounter === 120 && offset === true) {
       clearInterval(intervalId);
-      back();
-    } else updateCounter(h, m, s);
+
+      handleRedirectPrev();
+    } else updateTimer(element, h, m, s);
   }, 1000);
+};
+
+const injectHTML = (elementId, content) => {
+  document.getElementById(elementId).innerHTML = content;
+};
+
+const showElement = (elementId) => {
+  document.getElementById(elementId).style.display = 'flex';
+};
+
+const hideElement = (elementId) => {
+  document.getElementById(elementId).style.display = 'none';
+};
+
+const showAnimation = (elementId) => {
+  document.getElementById(elementId).classList.add('pending');
+};
+
+const hideAnimation = (elementId) => {
+  document.getElementById(elementId).classList.remove('pending');
+};
+
+const setLocalStorage = (tag, data) => {
+  localStorage.setItem(tag, JSON.stringify(data));
+};
+
+const replaceLocation = (URL) => {
+  window.location.href = URL;
 };
