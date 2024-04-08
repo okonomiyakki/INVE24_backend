4a4a89fb0402d82de88408f17dc7058f28699493
okonomiyakki <parkji12532@naver.com>
2024-04-08 20:54:25 +0900

    feat: 소환사 리그 및 계정 정보 응답 DTO

---
+++ src/auth/dto/summoner-league-info.dto.ts
@@ -0,0 +1,21 @@
+export class SummonerLeagueInfoDto {
+  encryptedSummonerId: string;
+
+  summonerName: string;
+
+  summonerTag: string;
+
+  summonerLevel: string;
+
+  profileIconId: number;
+
+  tier: string;
+
+  rank: string;
+
+  leaguePoints: number;
+
+  wins: number;
+
+  losses: number;
+}
